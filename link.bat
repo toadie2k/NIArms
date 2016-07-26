6b59bb1b7576c76b84c91794d88c929a749421db
toadie2k <toadie2k@gmail.com>
2016-07-26 12:24:25 +0800

    Updated Symlinks Batch File

--- link.bat
+++ link.bat
@@ -13,8 +13,21 @@ if not exist p:\ GOTO noP
 
 echo This program will create all necessary links to your P drive. Press enter to continue.
 pause
+
 mklink /J "p:\hlc_wp_m60E4" "%SOURCE%hlc_wp_m60E4"
-mklink /J "p:\hlc_wp_minigun" "%SOURCE%hlc_wp_minigun" 
+
+mklink /J "p:\hlc_wp_minigun" "%SOURCE%hlc_wp_minigun"
+
+mklink /J  "p:\hlc_wp_AR15" "%SOURCE%hlc_wp_AR15" 
+
+mklink /J  "p:\hlc_wp_springfield" "%SOURCE%hlc_wp_springfield" 
+
+mklink /J  "p:\hlc_wp_M14" "%SOURCE%hlc_wp_M14" 
+
+mklink /J  "p:\hlc_wp_FHAWC" "%SOURCE%hlc_wp_FHAWC" 
+
+mklink /J  "p:\hlc_wp_g3" "%SOURCE%hlc_wp_g3" 
+
 echo Complete
 pause
 GOTO quit
