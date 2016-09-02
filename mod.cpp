b385501ca4859e2bccff39ba2331c9b5ed778b76
toadie2k <toadie2k@gmail.com>
2016-09-02 11:57:20 +0800

    adding the @niarsenal generic mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "NIArsenal";
+picture = "NIArms1_ca.paa";
+hidePicture = "0";
+hideName = "0";
+actionName = "Website";
+action = "http://credmo.updatedtuesdays.com/tier1";
+description = "NIArsenal";
