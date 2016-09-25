ef601cedcf24d2d50c158f2322f2ee78cf50edb5
Robalo <rs@bzbit.com>
2016-09-25 16:19:47 +0300

    Tab2Space

--- Compatibility/hlc_rhs_compat_AUG/config.cpp
+++ Compatibility/hlc_rhs_compat_AUG/config.cpp
@@ -2,20 +2,20 @@
 
 class CfgPatches 
 {
-	class HLC_COMPAT_RHS_AUG
-	{
-		units[] = { };
-		weapons[] = { };
-		requiredVersion = 0.100000;
-		requiredAddons[] = { "A3_Weapons_F", "hlcweapons_core", "hlcweapons_AUG", "rhsusf_c_weapons" };
-		version = "1";
-		projectName = "HLCmod";
-		author = "Toadie";
-	};
+    class HLC_COMPAT_RHS_AUG
+    {
+        units[] = { };
+        weapons[] = { };
+        requiredVersion = 0.100000;
+        requiredAddons[] = { "A3_Weapons_F", "hlcweapons_core", "hlcweapons_AUG", "rhsusf_c_weapons" };
+        version = "1";
+        projectName = "HLCmod";
+        author = "Toadie";
+    };
 };
 class CfgWeapons
 {
-	class GrenadeLauncher;
+    class GrenadeLauncher;
     class UGL_F : GrenadeLauncher {
         magazines[] += { __40MM_M203_RHS_GRENADES };
     };
