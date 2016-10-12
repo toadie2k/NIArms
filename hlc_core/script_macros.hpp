1c8fcddaf5b3164ff36ad417d98c97287dae95d4
Robalo <rs@bzbit.com>
2016-10-12 18:35:52 +0300

    Add weaponholder macro, usage exemplified in AK pack

--- hlc_core/script_macros.hpp
+++ hlc_core/script_macros.hpp
@@ -18,6 +18,23 @@
 
 #define __OPTICSZOOM_1X opticsZoomMin = 0.25; opticsZoomMax = 1.25; opticsZoomInit = 0.75
 
+#define __WEAPONHOLDER(weapon,magazine,displayname,dlc,author,editorsubcat)    class Weapon_##weapon : Weapon_Base_F {\
+        dlc = "##dlc";\
+        scope = 2;\
+        scopeCurator = 2;\
+        displayName = "##displayname";\
+        author = "##author";\
+		editorCategory = "EdCat_Weapons";\
+		editorSubcategory = "EdSubcat_##editorsubcat";\
+        vehicleClass = "WeaponsPrimary";\
+        class TransportWeapons {\
+            class ##weapon { weapon = "##weapon"; count = 1; };\
+        };\
+        class TransportMagazines {\
+            class ##magazine { magazine = "##magazine"; count = 1; };\
+        };\
+    }
+
 #define __556STANAG_MAGS "hlc_30rnd_556x45_EPR", "hlc_30rnd_556x45_SOST", "hlc_30rnd_556x45_SPR", "hlc_30rnd_556x45_MDim", "hlc_30rnd_556x45_TDim", "hlc_50rnd_556x45_EPR", "hlc_30rnd_556x45_S"
 #define __556STANAG_BI_MAGS "30Rnd_556x45_Stanag", "30Rnd_556x45_Stanag_Tracer_Red", "30Rnd_556x45_Stanag_Tracer_Green", "30Rnd_556x45_Stanag_Tracer_Yellow", "30Rnd_556x45_Stanag_green", "30Rnd_556x45_Stanag_red"
 #define __556STANAG_RHS_MAGS "rhs_mag_30Rnd_556x45_Mk318_Stanag", "rhs_mag_30Rnd_556x45_Mk262_Stanag", "rhs_mag_30Rnd_556x45_M855A1_Stanag", "rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer", "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red", "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green", "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow", "rhs_mag_30Rnd_556x45_M200_Stanag"
