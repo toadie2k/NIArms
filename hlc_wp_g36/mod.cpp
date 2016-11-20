749ed94eca18fdecb6f7f7faefd693a838f9a06a
Robalo <rs@bzbit.com>
2016-11-20 17:27:34 +0200

    Remove confusing file

--- hlc_wp_g36/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = "NIArsenal: G36 Rifles";
-picture = "hlc_core\tex\NIArms1.paa";
-hidePicture = 0;
-hideName = 0;
-actionName = "Website";
-action = "http://credmo.updatedtuesdays.com/tier1";
-description = "NIArsenal: G36 Rifles";
