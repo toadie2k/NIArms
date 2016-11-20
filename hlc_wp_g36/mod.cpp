ae618780f52ad7825bf594d06cf0f301c8f63d56
Robalo <rs@bzbit.com>
2016-11-20 17:18:04 +0200

    Fix error caused by mod.cpp in G36 pack, ref #4

--- hlc_wp_g36/mod.cpp
+++ hlc_wp_g36/mod.cpp
@@ -1,7 +1,7 @@
 name = "NIArsenal: G36 Rifles";
 picture = "hlc_core\tex\NIArms1.paa";
-hidePicture = "false";
-hideName = "false";
+hidePicture = 0;
+hideName = 0;
 actionName = "Website";
 action = "http://credmo.updatedtuesdays.com/tier1";
 description = "NIArsenal: G36 Rifles";
