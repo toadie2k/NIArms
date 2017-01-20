9320369805852c443a9717cb4be4e5c3e6cf426e
Robalo <rs@bzbit.com>
2017-01-20 15:08:21 +0200

    Update G36 RHS compat (SAF)

--- Compatibility/hlc_RHS_compat_G36/config.cpp
+++ Compatibility/hlc_RHS_compat_G36/config.cpp
@@ -1,3 +1,5 @@
+#include "\hlc_core\script_macros.hpp"
+
 class CfgPatches 
 {
 	class HLC_COMPAT_RHS_G36
@@ -5,9 +7,22 @@ class CfgPatches
 		units[] = { };
 		weapons[] = { };
 		requiredVersion = 0.100000;
-		requiredAddons[] = { "A3_Weapons_F", "hlcweapons_core", "rhsusf_c_weapons", "hlcweapons_G36" };
+		requiredAddons[] = { "A3_Weapons_F", "hlcweapons_core", "rhssaf_c_weapons", "hlcweapons_G36" };
 		version = "1";
 		projectName = "HLCmod";
 		author = "Toadie";
+	};
+};
+
+class CfgWeapons
+{
+	class Rifle_Base_F;
+	class hlc_G36_base : Rifle_Base_F
+	{
+		magazines[] += { __556G36_RHS_MAGS };
+	};
+
+	class rhs_weap_g36_base : Rifle_Base_F {
+		magazines[] += { __556G36_MAGS };
 	};
 };
