fa69962404ca45a80c129747ef4f6230b1b6cca1
kerckasha <daza400@gmail.com>
2019-04-04 22:01:59 +1000

    ; help

--- hlc_core/RscIngameUI.hpp
+++ hlc_core/RscIngameUI.hpp
@@ -5,7 +5,7 @@ class RscInGameUI
 
   class Niarms_Rsc_AltReload_Zeroing: RscWeaponZeroing
   {
-    idd = 9640
+    idd = 9640;
     onLoad="['onLoad',_this,'RscUnitInfo','IGUI'] call (uinamespace getvariable 'BIS_fnc_initDisplay'); _this call Niarms_altReloads_fnc_postInit;";
   };
 };
